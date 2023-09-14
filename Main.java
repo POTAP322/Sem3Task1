5d5cb5c7da42016ef8b0d7f8165ec88155246067
POTAP322 <dragonkingmore@gmail.com>
2023-09-14 22:04:32 +0300

    first

---
+++ Main.java
@@ -0,0 +1,15 @@
+package Sem3Task1;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MainWindow mw= new MainWindow();
+        mw.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        mw.setSize(800,600);
+        mw.setVisible(true);
+
+
+
+    }
+}
---
+++ MainWindow.java
@@ -0,0 +1,7 @@
+package Sem3Task1;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+
+}
