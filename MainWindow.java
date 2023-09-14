19f84e21c201f2bfeb15ebb8cd3084f18daeb7a2
POTAP322 <dragonkingmore@gmail.com>
2023-09-14 22:35:42 +0300

    Cоздал DrawPanel для графики

--- MainWindow.java
+++ MainWindow.java
@@ -1,7 +1,13 @@
 package Sem3Task1;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainWindow extends JFrame {
+    private final DrawPanel dp;
 
+    public MainWindow() throws HeadlessException {
+        dp = new DrawPanel();
+        this.add(dp);
+    }
 }
