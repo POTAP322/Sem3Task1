b927e860cf8270aa164481ff67e83878b61a4e11
POTAP322 <dragonkingmore@gmail.com>
2023-09-16 19:48:28 +0300

    Эксперименты со всяким:)

--- DrawPanel.java
+++ DrawPanel.java
@@ -8,7 +8,29 @@ public class DrawPanel extends JPanel {
     }
 
     @Override
-    public void paint(Graphics g) {
-        super.paint(g);
+    public void paint(Graphics gr) {
+        super.paint(gr);
+        Graphics2D g = (Graphics2D) gr;
+        //g.rotate(Math.toRadians(90),400,300);
+        g.setPaint(new LinearGradientPaint(400,0,400,600,
+                new float[]{0.2f,0.7f},
+                new Color[]{Color.decode("#7FB4DC"),Color.decode("#D3F2FF")}));
+        g.fillRect(0,0,800,600);
+        //g.rotate(-Math.toRadians(90),400,300);
+//        g.setColor(Color.GREEN);
+//        g.drawLine(10, 10, 150, 150);
+//        final int Size = 2000;
+//        int[] x = new int[Size];
+//        int[] y  = new int[Size];
+//        for(int i = 0; i<Size;i++){
+//            x[i]=i*5;
+//            y[i] = 100+(int)(50*Math.cos(i*0.1));
+//        }
+//
+//        g.setStroke(new BasicStroke(5));
+//        g.drawPolygon(x,y,Size);
+//        g.setColor(Color.RED);
+//        g.fillPolygon(x,y,Size);
+
     }
 }
